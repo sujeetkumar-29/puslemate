5c9700d8dcdecc492ee6a1ab800fa645a37ed791
sujeet kumar <sujeetkr503@gmail.com>
2025-06-02 22:46:17 +0530

    feat: Implement doctor availability management and update context for admin panel

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,4 +1,20 @@
+import doctorModel from "../models/doctorModel";
 
 
+
+const changeAvailablity=async ( req,res)=>{
+    try {
+        const {docId}=req.body;
+        const docData= await doctorModel.findById(docId)
+        await doctorModel.findByIdAndUpdate(docId,{available:!docData.available})
+        res.json({success:true,message:"Availablity changed"})
+    } catch (error) {
+         console.log(error)
+            res.json({success:false,message:error.message})
+    }
+
+}
+
+export { changeAvailablity}
 
 
