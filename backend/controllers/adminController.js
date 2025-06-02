107593b3697fa0dbd000160b967ae6aa615b3950
sujeet kumar <sujeetkr503@gmail.com>
2025-06-02 17:13:54 +0530

    feat: Add API endpoint to retrieve all doctors for admin panel

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -100,5 +100,15 @@ const loginAdmin = async (req, res) => {
         res.json({ success: false, message: error.message || "Internal server error" });
     }
 }
+// API TO get all Doctors list for admin panel
+const allDoctors= async (req,res)=>{
+    try {
+            const doctors= await doctorModel.find({}).select("-password")
+            res.json({success:true,doctors})
+    } catch (error) {
+            console.log(error)
+            res.json({success:false,message:error.message})
+    }
+}
 
-export { addDoctor ,loginAdmin};
+export { addDoctor ,loginAdmin ,allDoctors} ;
