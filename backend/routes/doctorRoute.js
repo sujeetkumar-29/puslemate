b9296261ed90e486b0876129dabf422138a70fd5
sujeet kumar <sujeetkr503@gmail.com>
2025-06-07 17:56:33 +0530

    Add dashboard data retrieval and enhance DoctorContext and DoctorDashboard components

--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { doctorList ,loginDoctor,appointmentsDoctor,appointmentComplete,appointmentCancel} from "../controllers/doctorController.js";
+import { doctorList ,loginDoctor,appointmentsDoctor,appointmentComplete,appointmentCancel,doctorDashboard} from "../controllers/doctorController.js";
 import authDoctor from "../middlewares/authDoctor.js";
 
 const doctorRouter=express.Router()
@@ -9,6 +9,7 @@ doctorRouter.post("/login",loginDoctor)
 doctorRouter.get("/appointments",authDoctor,appointmentsDoctor)
 doctorRouter.post("/complete-appointment",authDoctor,appointmentComplete)
 doctorRouter.post("/cancel-appointment",authDoctor,appointmentCancel)
+doctorRouter.get("/dashboard",authDoctor,doctorDashboard)
 
 
 
